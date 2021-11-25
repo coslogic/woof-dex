ab99c51534ab94323ed8f9d850470ce3f8055a61
Azoth Zephyr <az@azothzephyr.xyz>
2021-11-25 14:27:15 -0800

    change some values from raydium to woofswap, attempt to change favicon (doesnt work still), add charting

--- src/pages/TradePage.tsx
+++ src/pages/TradePage.tsx
@@ -83,7 +83,7 @@ function TradePageInner() {
   });
 
   useEffect(() => {
-    document.title = marketName ? `${marketName} — Raydium` : 'Raydium';
+    document.title = marketName ? `${marketName} — WoofSwap` : 'WoofSwap';
   }, [marketName]);
 
   const changeOrderRef = useRef<
